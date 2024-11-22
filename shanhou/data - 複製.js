be3a8e1bf590430cd6a01ffd74b475bfde2340cd
Rein-hsym <lingzen0918@gmail.com>
2024-11-22 10:50:50 +0800

    shanhou

---
+++ shanhou/data - 複製.js
@@ -0,0 +1,398 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "山后民俗文化村",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": 0.155583045118302,
+        "pitch": -0.061455426509029465,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1225643936038896,
+          "pitch": 0.033961430589243946,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "山后民俗文化村-走道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": 0.21004728042279197,
+        "pitch": 0.029732484487063005,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.059528401094269,
+          "pitch": 0.32117944204938986,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.041778013684471205,
+          "pitch": 0.05787095703090728,
+          "rotation": 5.497787143782138,
+          "target": "2--"
+        },
+        {
+          "yaw": 0.6688316031458719,
+          "pitch": 0.031062561167985336,
+          "rotation": 5.497787143782138,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "山后民俗文化村-店家",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -0.19075280702138286,
+        "pitch": 0.18774890649093834,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4976265328579492,
+          "pitch": -0.021643456966042862,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 1.1698871717460175,
+          "pitch": -0.0400705372698944,
+          "rotation": 11.780972450961727,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.9233185337607903,
+          "pitch": 0.1781572775546323,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6990390829530995,
+          "pitch": -0.019302643546648568,
+          "title": "小吃店",
+          "text": "文化村中會有許多的店家。其中小吃店及紀念品店最多。"
+        }
+      ]
+    },
+    {
+      "id": "3--",
+      "name": "山后民俗文化村-閩式建築，古井",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23245946199394574,
+          "pitch": 0.16749000778752432,
+          "rotation": 0.7853981633974483,
+          "target": "4--"
+        },
+        {
+          "yaw": 2.4675736654635063,
+          "pitch": 0.24583231378619885,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4045249745992248,
+          "pitch": 0.480104634489404,
+          "title": "古井",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4469442483030441,
+          "pitch": 0.20832653706161608,
+          "title": "閩式建築",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4--",
+      "name": "山后民俗文化村-器物介紹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3856331059859368,
+          "pitch": -0.00010099050122391873,
+          "rotation": 5.497787143782138,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.303135313449273,
+          "pitch": 0.23748334246080205,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 0.05887390701560946,
+          "pitch": 0.3186090811289546,
+          "rotation": 0,
+          "target": "6--3"
+        },
+        {
+          "yaw": 3.102782615148727,
+          "pitch": 0.31174608226901057,
+          "rotation": 0,
+          "target": "5--2"
+        },
+        {
+          "yaw": -2.6743117098727716,
+          "pitch": 0.1481805769206943,
+          "rotation": 0.7853981633974483,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4421412092757997,
+          "pitch": 0.8710363163443446,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5296795208498501,
+          "pitch": 0.41648976168208485,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.654483516781376,
+          "pitch": 0.5497885392040232,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5--2",
+      "name": "山后民俗文化村-器物介紹2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -1.50598205197333,
+        "pitch": 0.561507036712829,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07028175901068856,
+          "pitch": 0.10695383573168904,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": -0.08333223161625192,
+          "pitch": -0.08228368058986923,
+          "rotation": 0,
+          "target": "6--3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6426869754151525,
+          "pitch": 0.44492761109131784,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.100386907145559,
+          "pitch": 0.5046328233412645,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.672839249519404,
+          "pitch": 0.504578514368692,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.39943702822798066,
+          "pitch": 0.28396033563483236,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6--3",
+      "name": "山后民俗文化村-器物介紹3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -1.478499164629934,
+        "pitch": 0.7906634248255173,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9406723137299355,
+          "pitch": 0.645008764800636,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 3.0081710336690124,
+          "pitch": 0.27556178105334084,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8918535967174108,
+          "pitch": 0.8400708029367436,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.863745208101971,
+          "pitch": 0.746287128849918,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4176173200547737,
+          "pitch": 0.4273863703607983,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
