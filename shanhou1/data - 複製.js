c22b3b0bf024c4c5ed0ccf97e7159c9c20b1d637
Rein-hsym <lingzen0918@gmail.com>
2024-11-29 11:59:21 +0800

    shanhou

---
+++ shanhou1/data - 複製.js
@@ -0,0 +1,356 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "山后民俗文化村",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": 0.18343766628102998,
+        "pitch": -0.07887209124352523,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08455050450828061,
+          "pitch": 0.14201839655853732,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.320486412822369,
+          "pitch": -0.2958355995857964,
+          "title": "山后民俗文化村",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "走道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -0.03407041723187376,
+        "pitch": -0.01017704403142261,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4123228655803466,
+          "pitch": 0.058865869491024014,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.5218020600316073,
+          "pitch": 0.03885442304822284,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.032806275428772,
+          "pitch": 0.3110877623252435,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "店家",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -0.06767603222019503,
+        "pitch": 0.16537696551061742,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.035616964638171,
+          "pitch": 0.21484082086766776,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.333971518466896,
+          "pitch": 0.03887078754561912,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.5667907349267942,
+          "pitch": 0.004500915806008976,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6732096432110453,
+          "pitch": 0.07044452306138993,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "閩式建築，古井",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -1.6057867211626053,
+        "pitch": 0.07333458199114418,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.895986592598237,
+          "pitch": 0.570308909368471,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.07223541225689445,
+          "pitch": 0.3169012234229953,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1837276431342865,
+          "pitch": 0.0696092173036007,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4673045692212172,
+          "pitch": 0.556646968085694,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "器物介紹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -1.6374348400038166,
+        "pitch": 0.656797234297759,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.604146789829386,
+          "pitch": 0.9298292573520328,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6453135742098457,
+          "pitch": 0.5242173232152307,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-2",
+      "name": "器物介紹2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -1.5493529903737144,
+        "pitch": 0.678629831703681,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8244828261926482,
+          "pitch": 0.19372865932657035,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.12256310710507634,
+          "pitch": 0.25856489154295836,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.08935523604606921,
+          "pitch": -0.010556608068700513,
+          "rotation": 0,
+          "target": "6-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.636594841912771,
+          "pitch": 0.43914192730538204,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-3",
+      "name": "器物介紹3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": -1.4854259449218965,
+        "pitch": 0.8157906812959723,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9901882149556975,
+          "pitch": 0.8693655722687801,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 3.052143789927353,
+          "pitch": 0.2965292680011782,
+          "rotation": 0,
+          "target": "5-2"
+        },
+        {
+          "yaw": -2.320659459633653,
+          "pitch": 0.2378573449922996,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.05446293733091068,
+          "pitch": 0.2259786602366347,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.881381831150204,
+          "pitch": 0.7412965651438679,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
