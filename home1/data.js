cfab7cb5c5ba526674ae41986de727bfc308c58d
Rein-hsym <lingzen0918@gmail.com>
2024-11-29 11:48:34 +0800

    home1

---
+++ home1/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "尚義機場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": 1.5780579649072388,
+        "pitch": -0.16792122651846952,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.9760949017548697,
+          "pitch": -0.4979587675066135,
+          "title": "金門-尚義機場",
+          "text": "金門唯一的機場，由於海港的船運只提供貨運，這便成了金門唯一能到本島的方式。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
