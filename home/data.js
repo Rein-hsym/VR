61a35c05a4823b78ddf468aec5ac70c394597ce2
Rein-hsym <lingzen0918@gmail.com>
2024-11-29 12:00:07 +0800

    VR

--- home/data.js
+++ home/data.js
@@ -20,65 +20,17 @@ var APP_DATA = {
       ],
       "faceSize": 832,
       "initialViewParameters": {
-        "yaw": 1.5551548514634623,
-        "pitch": -0.23406167619974383,
+        "yaw": 1.5780579649072388,
+        "pitch": -0.16792122651846952,
         "fov": 1.2599180821480807
       },
-      "linkHotspots": [
-        {
-          "yaw": -2.3528843642838204,
-          "pitch": -0.005804840605730277,
-          "rotation": 0,
-          "target": "1-"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": 1.8768631378324745,
-          "pitch": -0.3578320136893449,
-          "title": "金門航空站",
-          "text": "Text"
-        }
-      ]
-    },
-    {
-      "id": "1-",
-      "name": "機場外圓環",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 832,
-      "initialViewParameters": {
-        "yaw": 1.736431616572366,
-        "pitch": 0.1505849864216735,
-        "fov": 1.2599180821480807
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.5364437885880209,
-          "pitch": 0.05495544945061148,
-          "rotation": 0,
-          "target": "0-"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 2.113840807751692,
-          "pitch": -0.05885417289350414,
-          "title": "機場入口",
-          "text": "Text"
+          "yaw": 1.9760949017548697,
+          "pitch": -0.4979587675066135,
+          "title": "金門-尚義機場",
+          "text": "金門唯一的機場，由於海港的船運只提供貨運，這便成了金門唯一能到本島的方式。"
         }
       ]
     }
