bc4392e521db82a5762382053ed66f1556021b88
Rein-hsym <lingzen0918@gmail.com>
2024-12-27 11:19:54 +0800

    823

---
+++ 823/data.js
@@ -0,0 +1,398 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-823",
+      "name": "823戰史館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1270673364601791,
+          "pitch": 0.0009296041145177014,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.1664464104103054,
+          "pitch": 0.00343585900920651,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3741128360252972,
+          "pitch": -0.5712290050997098,
+          "title": "823戰史館",
+          "text": "823戰史館位於中央公園內。兩邊的牆壁上刻了許多劣勢的名字。館外還有各種戰車及淺挺可以拍照。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "入館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.492214677183627,
+          "pitch": 0.4105128813119414,
+          "rotation": 0.7853981633974483,
+          "target": "2-1"
+        },
+        {
+          "yaw": 1.4403284654951811,
+          "pitch": 0.26886506246284725,
+          "rotation": 0,
+          "target": "7-6"
+        },
+        {
+          "yaw": -2.804425041495275,
+          "pitch": 0.21751089536317814,
+          "rotation": 0,
+          "target": "0-823"
+        },
+        {
+          "yaw": 2.518905721682204,
+          "pitch": 0.19508944759282265,
+          "rotation": 0,
+          "target": "0-823"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1",
+      "name": "1區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07857671087921325,
+          "pitch": 0.3007033109952566,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0298555770672504,
+          "pitch": 0.3211247720639996,
+          "rotation": 0.7853981633974483,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1771905146251882,
+          "pitch": 0.11080507916961224,
+          "title": "第1區",
+          "text": "這裡有許多按比例做成的模型，包含軍艦、防空洞內部、食物及藥品等。"
+        },
+        {
+          "yaw": 0.5086701307980892,
+          "pitch": 0.11102716077015629,
+          "title": "第1區",
+          "text": "這台機器能讓遊客模擬戰鬥時如何在軍艦上發射砲彈。"
+        }
+      ]
+    },
+    {
+      "id": "3-2",
+      "name": "2區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.41250413563759736,
+          "pitch": 0.2584277031276745,
+          "rotation": 5.497787143782138,
+          "target": "2-1"
+        },
+        {
+          "yaw": 2.8300273475933198,
+          "pitch": 0.7842654911062414,
+          "rotation": 0,
+          "target": "4-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6142864898425238,
+          "pitch": 0.3412904742482432,
+          "title": "第2區",
+          "text": "這些是戰時曾用的各種砲彈的模型。"
+        }
+      ]
+    },
+    {
+      "id": "4-3",
+      "name": "3區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2027112904881232,
+          "pitch": 0.34895664641087976,
+          "rotation": 0,
+          "target": "3-2"
+        },
+        {
+          "yaw": 2.954767580997938,
+          "pitch": 0.8290288604737057,
+          "rotation": 0.7853981633974483,
+          "target": "5-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.075617018919976,
+          "pitch": -0.04138042598716396,
+          "title": "第3區",
+          "text": "這張圖描述了當時當地居民們在防空洞中的生活。"
+        }
+      ]
+    },
+    {
+      "id": "5-4",
+      "name": "4區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7604648308075515,
+          "pitch": 0.46394465688265285,
+          "rotation": 5.497787143782138,
+          "target": "4-3"
+        },
+        {
+          "yaw": -2.763992824530188,
+          "pitch": 0.4850648475040682,
+          "rotation": 0,
+          "target": "6-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4230656992409454,
+          "pitch": 0.18371237301753496,
+          "title": "第4區",
+          "text": "這是一個電動的小模型，掩飾當初軍艦從坑道中出海的過程。"
+        }
+      ]
+    },
+    {
+      "id": "6-5",
+      "name": "5區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1478404084234164,
+          "pitch": 0.2978587401960411,
+          "rotation": 0,
+          "target": "5-4"
+        },
+        {
+          "yaw": 3.0493788397689947,
+          "pitch": 0.39460284843030635,
+          "rotation": 0,
+          "target": "7-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4703951655453924,
+          "pitch": 0.21573172187875045,
+          "title": "第5區",
+          "text": "這區的看板描述了兩岸當初打仗時做的部屬工程及戰況。"
+        }
+      ]
+    },
+    {
+      "id": "7-6",
+      "name": "6區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "yaw": 0.8775979861520096,
+        "pitch": -0.08262530456332229,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0308295035285084,
+          "pitch": 0.3876318120406008,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.4083061232100746,
+          "pitch": 0.35628799308721426,
+          "rotation": 0,
+          "target": "6-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2460526001936127,
+          "pitch": 0.004067211447615904,
+          "title": "第6區",
+          "text": "這張圖展示了二戰後各國的情勢及關係。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
