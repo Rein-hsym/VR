28d22deb1714774081cb3355fa9cddbf522d6fe6
Rein-hsym <lingzen0918@gmail.com>
2025-01-10 10:26:28 +0800

    Create data - 複製.js

---
+++ shanhou/data - 複製.js
@@ -0,0 +1,357 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "山后民俗文化村",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15385107186461688,
+          "pitch": 0.004005228427079288,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.28681966612615284,
+          "pitch": -0.3285175115791006,
+          "title": "山后民俗文化村",
+          "text": "原建築建於1900年，現已改為博物館，同時也是金門首個成為關瓜井點的聚落。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "走道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.072616878918506,
+          "pitch": 0.3209094290434322,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.14682032743937157,
+          "pitch": 0.030091900168763175,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.4724175258279626,
+          "pitch": 0.008204186758860033,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "店家",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.991734602986673,
+          "pitch": 0.21776257287891276,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.06463219712074952,
+          "pitch": -0.02811252050216062,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4972108014648242,
+          "pitch": 0.02239757109738605,
+          "title": "小吃店",
+          "text": "民俗文化村內有許多小吃店，也有一些特產可供選購。"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "閩式建築，古井",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9046780506006016,
+          "pitch": 0.2828972117350883,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.1792247581479227,
+          "pitch": 0.3258359677657925,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.576446098855099,
+          "pitch": 0.4994251231599893,
+          "title": "古井",
+          "text": "原本是作為汲水用的井，現在改造成魚池。"
+        },
+        {
+          "yaw": -1.2142233957334838,
+          "pitch": -0.004038738810706377,
+          "title": "閩式建築",
+          "text": "閩式建築的特色有幾個:斜屋頂、燕尾脊，且通常只有一層樓。在金門各地經常能見到。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "器物介紹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1770394261632049,
+          "pitch": 0.21540820951377526,
+          "rotation": 0,
+          "target": "6-3"
+        },
+        {
+          "yaw": 2.9898505232520103,
+          "pitch": 0.28297590275073503,
+          "rotation": 0,
+          "target": "5-2"
+        },
+        {
+          "yaw": -0.07963753555204889,
+          "pitch": 0.0514914927976875,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.8588802134239124,
+          "pitch": 0.10541804118829745,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5939720884591377,
+          "pitch": 0.8402588813801604,
+          "title": "石磨",
+          "text": "石磨可以磨穀去殼，也可以磨米成粉，便能更容易將其加工成糕或是粿等食品。"
+        },
+        {
+          "yaw": -2.6568211057215603,
+          "pitch": 0.5386033871003484,
+          "title": "水缽",
+          "text": "水缽的功能很多。比方說可以煮茶、當成洗手台，有些人也會將它當成魚缸來養魚。"
+        }
+      ]
+    },
+    {
+      "id": "5-2",
+      "name": "器物介紹2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08643566656203738,
+          "pitch": 0.28557303067755413,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.03714328726369409,
+          "pitch": 0.01262063974550287,
+          "rotation": 0,
+          "target": "6-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7757663331336957,
+          "pitch": 0.5014897726690553,
+          "title": "小型石磨",
+          "text": "用途跟前述的大抵相同，只是一次能磨出的量較有限。"
+        }
+      ]
+    },
+    {
+      "id": "6-3",
+      "name": "器物介紹3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 832,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.819423880021086,
+          "pitch": 0.6587746126145628,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.949984938395966,
+          "pitch": 0.27473178231659645,
+          "rotation": 0,
+          "target": "6-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9462880960777529,
+          "pitch": 0.7911336511638893,
+          "title": "石缽",
+          "text": "石缽主要用來將米、花生搗成更細的粉，也能用來製作草藥。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
